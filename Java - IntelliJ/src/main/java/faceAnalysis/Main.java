eb4319346696c4282970cdfd8b13c7dbdd5400a8
vilmosvegvari <vegvari.vili@gmail.com>
2019-03-12 02:32:21 +0100

    No it can analyse 1 picture from command line

--- Java - IntelliJ/src/main/java/faceAnalysis/Main.java
+++ Java - IntelliJ/src/main/java/faceAnalysis/Main.java
@@ -1,17 +1,36 @@
 package faceAnalysis;
 
+import logic.Controller;
+import logic.RequestHandler;
 import view.UI;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 public class Main {
 
 
 	public static void main(String[] args) {
 
-		if (args.length == 0) {
+		if (args.length == 0 || args.length>2 ) {
             UI ui = new UI();
             ui.go();
-
         }
-	}
+        try {
+            Controller c = new Controller(new RequestHandler());
+            if (args.length == 1) {
+                c.AnalyseLocalPicture(new File(args[0]));
+            }
+            if (args.length == 2) {
+                c.CompareTwoPictures(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toString()+ args[0]),
+                        new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath() + args[1]));
+            }
+        }
+        catch (URISyntaxException e){
+		    e.getMessage();
+            System.out.println("Probléma a fálj beolvasása közben.");
+        }
+
+    }
 
 }
