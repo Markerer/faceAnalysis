53517911d7a02d2f65524c40f8b622e9062cab3a
vilmosvegvari <vegvari.vili@gmail.com>
2019-03-23 14:33:12 +0100

    Now on HomePage we can upload files

--- Spring/src/main/java/com/faceanalysis/faceAnalysis/FaceAnalysisApplication.java
+++ Spring/src/main/java/com/faceanalysis/faceAnalysis/FaceAnalysisApplication.java
@@ -7,13 +7,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-
+@EnableConfigurationProperties(StorageProperties.class)
 public class FaceAnalysisApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FaceAnalysisApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 
 }
