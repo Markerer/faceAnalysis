1973128002ed1a50ba1d5a3d3f774b4c5aa8e401
vilmosvegvari <vegvari.vili@gmail.com>
2019-03-23 12:02:25 +0100

    Spring boot starter

---
+++ Spring/src/main/java/com/faceanalysis/faceAnalysis/FaceAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.faceanalysis.faceAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAnalysisApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/com/faceanalysis/faceAnalysis/FaceAnalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faceanalysis.faceAnalysis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaceAnalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
