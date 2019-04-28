0e96c1044cd8392a7aa2df285938629029d094f3
vilmosvegvari <vegvari.vili@gmail.com>
2019-04-29 00:29:43 +0200

    doneso

--- Frontend/FaceAnalysis/src/app/face-analysis/face-analysis.component.ts
+++ Frontend/FaceAnalysis/src/app/face-analysis/face-analysis.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../main.service';
-
 import { WebcamImage } from 'ngx-webcam';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
